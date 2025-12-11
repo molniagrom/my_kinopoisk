89c4966267c03ad84a75058ca5dc0f026871beae
molnia_grom <mgrom250@gmail.com>
2025-12-11 11:20:52 +0200

    add env local

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,12 @@
 export const Main = () => {
+
     return (
-        <h1>Main</h1>
+
+        <>
+            <h1>Main</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit beatae repellat maxime consequatur esse hic, quas laboriosam necessitatibus, dicta sit aliquam ex exercitationem saepe illo fugiat atque minus perferendis officia?</p>
+        </>
+
     );
 };
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,16 +3,18 @@ import { Path } from '../../common/routing/Routing';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+// import { ReactComponent as MySvgIcon } from '../../assets/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg';
 
 export const Header = () => {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Button color="inherit" component={Link} to={Path.Main}>Main</Button>
-                <Button color="inherit" component={Link} to={Path.CategoryMovies}>Category Movies</Button>
-                <Button color="inherit" component={Link} to={Path.FilteredMovies}>Filtered Movies</Button>
-                <Button color="inherit" component={Link} to={Path.Search}>Search</Button>
-                <Button color="inherit" component={Link} to={Path.Favorites}>Favorites</Button>
+                {/* <MySvgIcon /> */}
+                <Button color="inherit" component={Link} to={Path.Main} sx={{ '&:hover': { color: '#e0e0e0' } }}>Main</Button>
+                <Button color="inherit" component={Link} to={Path.CategoryMovies} sx={{ '&:hover': { color: '#e0e0e0' } }}>Category Movies</Button>
+                <Button color="inherit" component={Link} to={Path.FilteredMovies} sx={{ '&:hover': { color: '#e0e0e0' } }}>Filtered Movies</Button>
+                <Button color="inherit" component={Link} to={Path.Search} sx={{ '&:hover': { color: '#e0e0e0' } }}>Search</Button>
+                <Button color="inherit" component={Link} to={Path.Favorites} sx={{ '&:hover': { color: '#e0e0e0' } }}>Favorites</Button>
             </Toolbar>
         </AppBar>
     );
