2354147e22957ea5c718a56be5d62fea95add148
molnia_grom <mgrom250@gmail.com>
2026-02-15 11:50:02 +0200

    add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-// import './App.css'
+import './App.css'
 import {Routing} from "./common/routing/Routing.tsx";
 import Header from "./common/components/header/Header.tsx";
+import { Footer } from "./common/components/Footer/Footer.tsx";
 
 function App() {
 
     return (
-        <>
+        <div className="app">
             <Header/>
-            <main>
+            <main className="appMain">
                 <Routing/>
             </main>
-        </>
+            <Footer />
+        </div>
     )
 }
 
