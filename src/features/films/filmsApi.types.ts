225ca06a88859613bc3a9adb19d107147e33c27a
molnia_grom <mgrom250@gmail.com>
2026-03-20 11:52:31 +0200

    fix bag with build

--- src/features/films/filmsApi.types.ts
+++ src/features/films/filmsApi.types.ts
@@ -3,7 +3,7 @@
 export type Movie = {
   adult: boolean;
   backdrop_path: string | null;
-  genre_ids: number[];
+  genre_ids?: number[];
   id: number;
   original_language: string;
   original_title: string;
