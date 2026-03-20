edb0b97f77c048cd725f960dad8ab17c5b38be40
molnia_grom <mgrom250@gmail.com>
2026-03-20 15:31:25 +0200

    We made a few minor adjustments to the styles

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -43,7 +43,7 @@ export const Header = () => {
   const sessionId = useAppSelector(selectAuthSessionId);
   const isDarkTheme = themeMode === 'dark';
   const navigate = useNavigate();
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(true);
   const [searchValue, setSearchValue] = useState('');
   const [createRequestToken] = useCreateRequestTokenMutation();
   const { data: accountData } = useGetAccountQuery(
@@ -108,126 +108,122 @@ export const Header = () => {
 
   return (
     <AppBar className={s.header} position="static">
-      <Toolbar className={s.toolbar}>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-          <Box component="img" src={tmdbLogo} alt="TMDB Logo" className={s.logo} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
-            {navLinks.map((link) => (
-              <Button key={link.path} color="inherit" className={s.navButton} component={Link} to={link.path}>
-                {link.label}
-              </Button>
-            ))}
+      {isSearchOpen ? (
+        <Toolbar className={s.toolbar}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <Box component="img" src={tmdbLogo} alt="TMDB Logo" className={s.logo} />
+            <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
+              {navLinks.map((link) => (
+                <Button key={link.path} color="inherit" className={s.navButton} component={Link} to={link.path}>
+                  {link.label}
+                </Button>
+              ))}
+            </Box>
           </Box>
-        </Box>
-        <Box className={s.rightArea} sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', gap: 2 }}>
-          <Box className={isSearchOpen ? s.actionsHidden : s.actions} aria-hidden={isSearchOpen}>
-            <MyAvatar src={avatarSrc} alt="User Avatar" size={40} />
-            <Button className={s.logoutButton} variant="contained" onClick={handleAuthClick}>
-              {isAuthorized ? 'log out' : 'log in'}
-            </Button>
-            {/*<Button color="inherit" variant="outlined" >*/}
-            {/*  {isSearchOpen ? 'Close Search' : 'Open Search'}*/}
-            {/*</Button>*/}
-            <SearchIcon sx={{ fontSize: 30, cursor: 'pointer' }} onClick={() => setIsSearchOpen((prev) => !prev)} />
-
-            <NotificationsIcon sx={{ fontSize: 30, cursor: 'pointer' }} />
-            <IconButton
-              color="inherit"
-              aria-label="toggle theme"
-              onClick={() => dispatch(toggleTheme())}
-              size="large"
-            >
-              {isDarkTheme ? <LightModeIcon /> : <Brightness4Icon />}
-            </IconButton>
-          </Box>
-        </Box>
-        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-          <IconButton size="large" edge="end" color="inherit" aria-label="menu" onClick={handleMenu}>
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={open}
-            onClose={handleClose}
-            sx={{ mt: '40px' }}
-          >
-            {navLinks.map((link) => (
-              <MenuItem key={link.path} onClick={handleClose} component={Link} to={link.path}>
-                {link.label}
-              </MenuItem>
-            ))}
-
-            <MenuItem
-              onClick={() => {
-                handleClose();
-                setIsSearchOpen(true);
-              }}
-            >
-              <SearchIcon sx={{ mr: 1 }} /> Search
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <NotificationsIcon sx={{ mr: 1 }} /> Notifications
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <MyAvatar src={avatarSrc} alt="User Avatar" size={24} />
-              <span style={{ marginLeft: '8px' }}>Profile</span>
-            </MenuItem>
-            <MenuItem
-              onClick={() => {
-                dispatch(toggleTheme());
-                handleClose();
-              }}
-            >
-              {isDarkTheme ? (
-                <LightModeIcon sx={{ mr: 1 }} />
-              ) : (
-                <Brightness4Icon sx={{ mr: 1 }} />
-              )}
-              <span style={{ marginLeft: '8px' }}>Toggle theme</span>
-            </MenuItem>
-            <MenuItem
-              onClick={() => {
-                handleClose();
-                handleAuthClick();
-              }}
-            >
-              <Button className={s.logoutButton} variant="contained" size="small" fullWidth>
+          <Box className={s.rightArea} sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', gap: 2 }}>
+            <Box className={s.actions}>
+              <MyAvatar src={avatarSrc} alt="User Avatar" size={40} />
+              <Button className={s.logoutButton} variant="contained" onClick={handleAuthClick}>
                 {isAuthorized ? 'log out' : 'log in'}
               </Button>
-            </MenuItem>
-          </Menu>
+              <SearchIcon sx={{ fontSize: 30, cursor: 'pointer' }} onClick={() => setIsSearchOpen(false)} />
+              <NotificationsIcon sx={{ fontSize: 30, cursor: 'pointer' }} />
+              <IconButton
+                color="inherit"
+                aria-label="toggle theme"
+                onClick={() => dispatch(toggleTheme())}
+                size="large"
+              >
+                {isDarkTheme ? <LightModeIcon /> : <Brightness4Icon />}
+              </IconButton>
+            </Box>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton size="large" edge="end" color="inherit" aria-label="menu" onClick={handleMenu}>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+              sx={{ mt: '40px' }}
+            >
+              {navLinks.map((link) => (
+                <MenuItem key={link.path} onClick={handleClose} component={Link} to={link.path}>
+                  {link.label}
+                </MenuItem>
+              ))}
+
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  setIsSearchOpen(false);
+                }}
+              >
+                <SearchIcon sx={{ mr: 1 }} /> Search
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <NotificationsIcon sx={{ mr: 1 }} /> Notifications
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <MyAvatar src={avatarSrc} alt="User Avatar" size={24} />
+                <span style={{ marginLeft: '8px' }}>Profile</span>
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  dispatch(toggleTheme());
+                  handleClose();
+                }}
+              >
+                {isDarkTheme ? (
+                  <LightModeIcon sx={{ mr: 1 }} />
+                ) : (
+                  <Brightness4Icon sx={{ mr: 1 }} />
+                )}
+                <span style={{ marginLeft: '8px' }}>Toggle theme</span>
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  handleAuthClick();
+                }}
+              >
+                <Button className={s.logoutButton} variant="contained" size="small" fullWidth>
+                  {isAuthorized ? 'log out' : 'log in'}
+                </Button>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      ) : (
+        <Box className={s.searchRowOpen}>
+          <Box sx={{ color: 'red', fontWeight: 700, mr: 2 }}>SEARCH OPEN</Box>
+          <div className={s.searchFormWrap}>
+            <SearchForm
+              value={searchValue}
+              onChange={setSearchValue}
+              onSubmit={handleSearchSubmit}
+              onClear={handleSearchClear}
+              placeholder="Search movies..."
+              buttonLabel="Go"
+              suggestions={suggestions}
+            />
+          </div>
+          <IconButton aria-label="close search" onClick={() => setIsSearchOpen(true)} size="small">
+            <CloseIcon />
+          </IconButton>
         </Box>
-      </Toolbar>
-      <Box
-        className={isSearchOpen ? s.searchRowOpen : s.searchRow}
-        sx={{ display: isSearchOpen ? 'flex' : 'none' }}
-      >
-        <Box sx={{ color: 'red', fontWeight: 700, mr: 2 }}>SEARCH OPEN</Box>
-        <div className={s.searchFormWrap}>
-          <SearchForm
-            value={searchValue}
-            onChange={setSearchValue}
-            onSubmit={handleSearchSubmit}
-            onClear={handleSearchClear}
-            placeholder="Search movies..."
-            buttonLabel="Go"
-            suggestions={suggestions}
-          />
-        </div>
-        <IconButton aria-label="close search" onClick={() => setIsSearchOpen(false)} size="small">
-          <CloseIcon />
-        </IconButton>
-      </Box>
+      )}
     </AppBar>
   );
 };
